dab04a0c406c981321c93c6050952bfa6c89993f
sahithya007 <97717125+sahithya007@users.noreply.github.com>
2024-10-14 17:55:12 +0530

    Delete FinanceManagement directory

--- FinanceManagement/exception/ExpenseNotFoundException.py
+++
@@ -1,4 +0,0 @@
-class ExpenseNotFoundException(Exception):
-    def __init__(self, message="Expense not found"):
-        self.message = message
-        super().__init__(self.message)
--- FinanceManagement/util/DBPropertyUtil.py
+++
@@ -1,6 +0,0 @@
-def get_property_string():
-    return 'Driver={SQL Server};' \
-           'Server=sahithya;' \
-           'Database=Finance_Mgmt;' \
-           'Trusted_Connection=yes;'
-
